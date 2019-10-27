7ea670d1eee187cbbee8f64be002e05f84d1fd56
Артём Полянский <lazemir@yandex.ru>
2019-10-27 03:33:16 +0300

    Игра доработана. Теперь не возникает деления на ноль в вертикальных положениях пушки, сама пушка может поворачиваться и стрелять куда угодно, также стреляет она более естестенно. Добавлена поддержка нескольких игр подряд

--- lab8/gun.py
+++ lab8/gun.py
@@ -17,7 +17,7 @@ canv.pack(fill=tk.BOTH, expand=1)
 
 
 class ball():
-	def __init__(self, x=40, y=450):
+	def __init__(self, x, y):
 		""" Конструктор класса ball
 
 		Args:
@@ -75,10 +75,20 @@ class ball():
 			return False
 	
 	def walltest(self, wall1_x, wall1_y, wall2_x, wall2_y):
-		if self.x - self.r <= wall1_x or self.x + self.r >= wall2_x:
+		res = True
+		if self.x - self.r <= wall1_x and self.vx < 0:
 			self.vx *= -1 * N
-		if self.y - self.r <= wall1_y or self.y + self.r >= wall2_y:
+		elif self.x + self.r >= wall2_x and self.vx > 0:
+			self.vx *= -1 * N
+		if self.y - self.r <= wall1_y and self.vy < 0:
 			self.vy *= -1 * N
+		elif self.y + self.r >= wall2_y and self.vy > 0:
+			self.vy *= -1 * N
+		else:
+			res = False
+		return res
+
+
 	
 	def __del__(self):
 		canv.delete(self.id)
@@ -101,9 +111,8 @@ class gun():
 		"""
 		global balls, bullet
 		bullet += 1
-		new_ball = ball()
+		new_ball = ball(self.xe, self.ye)
 		new_ball.r += 5
-		self.an = math.atan((event.y-new_ball.y) / (event.x-new_ball.x))
 		new_ball.vx = V / 100 * self.f2_power * math.cos(self.an)
 		new_ball.vy = V / 100 * self.f2_power * math.sin(self.an)
 		balls += [new_ball]
@@ -113,15 +122,23 @@ class gun():
 	def targetting(self, event=0):
 		"""Прицеливание. Зависит от положения мыши."""
 		if event:
-			self.an = math.atan((event.y-450) / (event.x-20))
+			if (event.x-20) > 0:
+				self.an = math.atan((event.y-450) / (event.x-20))
+			elif (event.x-20) == 0:
+				if (event.y-450) > 0:
+					self.an = math.pi/2
+				else:
+					self.an = -math.pi / 2
+			else:
+				self.an = math.pi + math.atan((event.y - 450) / (event.x - 20))
 		if self.f2_on:
 			canv.itemconfig(self.id, fill='orange')
 		else:
 			canv.itemconfig(self.id, fill='black')
-		canv.coords(self.id, 20, 450,
-					20 + max(self.f2_power, 20) * math.cos(self.an),
-					450 + max(self.f2_power, 20) * math.sin(self.an)
-					)
+
+		self.xe = 20 + max(self.f2_power, 20) * math.cos(self.an)
+		self.ye = 450 + max(self.f2_power, 20) * math.sin(self.an)
+		canv.coords(self.id, 20, 450, self.xe, self.ye)
 
 	def power_up(self):
 		if self.f2_on:
@@ -134,6 +151,8 @@ class gun():
 
 class target():
 	points = 0
+	id_points = canv.create_text(30, 30, text=points, font='28')
+
 	def __init__(self):
 		self.live = 1
 		x = self.x = rnd(600, 780)
@@ -141,57 +160,53 @@ class target():
 		r = self.r = rnd(2, 50)
 		color = self.color = 'red'
 		self.id = canv.create_oval(x-r, y-r, x+r, y+r, fill=color)
-		self.id_points = canv.create_text(30,30,text = self.points,font = '28')
+		#target.id_points = canv.create_text(30,30,text = self.points,font = '28')
+		canv.itemconfig(target.id_points,text = target.points,font = '28')
 
 	def hit(self, points=1):
 		"""Попадание шарика в цель."""
 		canv.coords(self.id, -10, -10, -10, -10)
-		self.points += points
+		target.points += points
 		canv.itemconfig(self.id_points, text=self.points)
 
-
-
-screen1 = canv.create_text(400, 300, text='', font=('', 28))
-g1 = gun()
-bullet = 0
-balls = []
-
-
 def new_game(event=''):
-	global gun, screen1, balls, bullet
-	t1 = target()
+	global balls, bullet, sreen1, canv
+	g1 = gun()
 	bullet = 0
+	t1 = target()
 	balls = []
 	canv.bind('<Button-1>', g1.fire2_start)
 	canv.bind('<ButtonRelease-1>', g1.fire2_end)
 	canv.bind('<Motion>', g1.targetting)
 
-
-	z = 0.03
+	canv.itemconfig(screen1, text='')
 	t1.live = 1
 	while t1.live or balls:
 		for b in list(balls):
 			b.move()
-			b.walltest(0, 0, 800, 600)
-			#if abs(b.vy) < 1:
-			#	print(b.vy)
+			if b.walltest(0,0, 800, 600) and abs(b.vy) <= A or not t1.live:
 				balls.remove(b)
 			if b.hittest(t1) and t1.live:
-				bullet += 1
 				t1.live = 0
 				t1.hit()
 				canv.bind('<Button-1>', '')
 				canv.bind('<ButtonRelease-1>', '')
-				canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
+				if 10 <= bullet % 100 <= 20 or 5 <= bullet % 10 <= 9 or bullet % 10 == 0:
+					shot = ' выстрелов'
+				elif bullet % 10 == 1:
+					shot = ' выстрел'
+				else:
+					shot = ' выстрела'
+				canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + shot)
 		canv.update()
 		time.sleep(0.03)
 		g1.targetting()
 		g1.power_up()
-	canv.itemconfig(screen1, text='')
 	canv.delete(g1.id)
-	root.after(750, new_game)
+	root.after(2000, new_game)
 
+screen1 = canv.create_text(400, 300, text='', font=('', 28))
 
 new_game()
 
-mainloop()
+root.mainloop()
